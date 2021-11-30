01bd49f3ce58b89071dc28505cf79c3967483428
papifull <65138443+papifull@users.noreply.github.com>
2021-11-30 23:34:47 +0100

    Update script3.js

--- appWeb/Controller/script3.js
+++ appWeb/Controller/script3.js
@@ -3,7 +3,7 @@
 		var rettVal = confirm("Voulez vous supprimer cette categorie?");
 		if (rettVal == true) {
 			var va=va1;
-			document.location.href="http://localhost/Projet/Modele/supprim.php?id="+va;
+			document.location.href="http://localhost/appWeb/Modele/supprim.php?id="+va;
 			return true;
 		}
 		else{
@@ -15,7 +15,7 @@
 		var rettVal = confirm("Voulez vous modifier cette categorie?");
 		if (rettVal == true) {
 			var va=va1;
-			document.location.href="http://localhost/Projet/Modele/modif.php?idd="+va;
+			document.location.href="http://localhost/appWeb/Modele/modif.php?idd="+va;
 			return true;
 		}
 		else{
